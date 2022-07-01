1e42c3993406a35c4655b1bb594eed1be9d76000
moonjs0113 <littlemoom@naver.com>
2022-07-01 22:49:02 +0900

    init Repo

---
+++ NetworkStudyApp/NetworkStudyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkStudyAppApp.swift
+//  NetworkStudyApp
+//
+//  Created by Moon Jongseek on 2022/07/01.
+//
+
+import SwiftUI
+
+@main
+struct NetworkStudyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
